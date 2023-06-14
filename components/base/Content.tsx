430fdb6965c8256fa912ad5d2e238b39476cc131
FlugHeli <anselm@LAPTOP-JQASEHJV>
2023-06-14 11:01:54 +0200

    transfered whole code

---
+++ components/base/Content.tsx
@@ -0,0 +1,21 @@
+import React from "react";
+
+export const Content = () => {
+    return (
+        <div className="content">
+            <div className="content__main">
+                <div className="content__heading">may the force be with you</div>
+                <div className="content__text">
+                    To crush the rebellion once and for all, the EMPIRE is constructing a sinister and new battle station.
+                    <br />Powerful enough to destroy an entire
+                    planet, its completion spells certain doom.
+                </div>
+                <div className="content__buttons">
+                <button className="button" type="button"><span></span>watch more</button>
+                <button className="button" type="button"><span></span>join us</button>
+                </div>
+            </div>
+        </div>
+    )
+  }
+  
--- components/common/Header.tsx
+++ components/common/Header.tsx
@@ -1,22 +1,26 @@
 import React from 'react'
+import Image from 'next/image'
 
 export const Header = () => {
   return (
     <div className="header">
-      <div className=" line line--short"></div>
-      <div className="line"></div>
-      <div className="header__main">
-        <div className="header__font">Roxy&apos;s</div>
-        <div className="header__logo">
-          <img
-            src="logo_primary_white.svg"
-            alt="Logo"
-            className="header__logo"
-          />
-        </div>
-        <div className="header__font">Burger</div>
+      <div className="header__logo">
+        <Image
+          src="/StarWarsTransparent.png"
+          alt="Star Wars Logo"
+          width={100}
+          height={50}
+        />
       </div>
-      <div className="line"></div>
+      <div className="header__navbar">
+        <ul>
+          <li><a href="">Sith</a></li>
+          <li><a href="">Jedi</a></li>
+          <li><a href="">Locations</a></li>
+          <li><a href="">Weapons</a></li>
+          <li><a href="">Vehicles</a></li>
+        </ul>
+      </div>
     </div>
   )
 }
